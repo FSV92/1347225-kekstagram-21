4745422edb8a61290c50204d18767cac7fc829b6
Sergey <filonov_sv@mail.ru>
2020-10-16 16:47:00 +0800

    17. Личный проект: надо подкачаться (часть 2)

--- js/backend.js
+++ js/backend.js
@@ -2,22 +2,43 @@
 
 (function () {
   const URL_GET = `https://21.javascript.pages.academy/kekstagram/data`;
+  const URL_POST = `https://21.javascript.pages.academy/kekstagram`;
+  const STATUS_OK = 200;
+
   let getPictures = function (onSuccess, onError) {
     let xhr = new XMLHttpRequest();
     xhr.responseType = `json`;
 
     xhr.addEventListener(`load`, function () {
-      if (xhr.status === 200) {
+      if (xhr.status === STATUS_OK) {
         onSuccess(xhr.response);
       } else {
-        onError(`Произошла ошибка: ` + xhr.status + ` ` + xhr.text);
+        onError(`Произошла ошибка получения: ` + xhr.status + ` ` + xhr.text);
       }
     });
 
     xhr.open(`GET`, URL_GET);
     xhr.send();
   };
+
+  let sendForm = function (data, onSuccess, onError) {
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = `json`;
+
+    xhr.addEventListener(`load`, function () {
+      if (xhr.status === STATUS_OK) {
+        onSuccess();
+      } else {
+        onError();
+      }
+    });
+
+    xhr.open(`POST`, URL_POST);
+    xhr.send(data);
+  };
+
   window.backend = {
-    getPictures
+    getPictures,
+    sendForm,
   };
 })();
